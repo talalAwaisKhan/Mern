58193de5f494ca642937d8581431f4fce3b45840
talalAwaisKhan <92671778+talalAwaisKhan@users.noreply.github.com>
2021-11-19 16:32:29 +0500

    config

---
+++ db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async ()=>{
+  try{
+    await mongoose.connect(db);
+    useNewUrlParser: true;
+    useCreateIndex: true;
+    console.log('DataBase Connected');
+  }
+  catch(err){
+    console.error(err.message);
+    //exit pogaam with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
